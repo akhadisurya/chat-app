6e8e321d0b45fe2f4231be4982f59f571746cf37
Arvin <akhadisurya@gmail.com>
2017-01-03 15:19:16 -0800

    chat room works

--- server.js
+++ server.js
@@ -20,4 +20,9 @@ io.sockets.on('connection', function(socket){
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length)
   });
+
+  //Send Message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data})
+  })
 });

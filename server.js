6a42aa77d56c7f16c99ac7a5cfc7244be6dee635
Arvin <akhadisurya@gmail.com>
2017-01-11 14:29:41 -0800

    create socket on get users

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ io.sockets.on('connection', function(socket){
 
   //Disconnect
   socket.on('disconnect', function(data){
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames;
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length)
   });
@@ -34,5 +36,7 @@ io.sockets.on('connection', function(socket){
     updateUsernames();
   })
 
-
+  function updateUsernames(){
+    io.sockets.emit('get users', usernames);
+  }
 });
